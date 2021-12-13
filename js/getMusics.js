da9cca7c8e93200e9176e438961f629ca665497b
Xwilarg <xwilarg@yahoo.fr>
2021-12-14 00:17:41 +0100

    Display queue

--- js/getMusics.js
+++ js/getMusics.js
@@ -9,13 +9,24 @@ let playlist = [];
 
 // Play the next song of the playlist
 function nextSong() {
+    // Update playlist text
+    document.getElementById("playlist").innerHTML =
+        `${playlist.length} song${playlist.length > 1 ? 's' : ''} queued:<br/>` +
+        playlist
+            .slice(0, 3)
+            .map(x => sanitize(json[x].name))
+            .join("<br/>");
+
+    // Select current song and move playlist forward
     let elem = json[0];
     playlist.shift();
 
+    // Load song and play it
     let player = document.getElementById("player");
     player.src = `${url}/data/${elem.path}`;
     player.play();
 
+    // Color the currently played song
     let current = document.getElementsByClassName("current");
     if (current.length > 0) {
         current[0].classList.remove("current");
@@ -72,7 +83,10 @@ async function loadPage() {
     player.volume = 0.1; // Base volume is way too loud
     // When song end, we start the next one
     player.addEventListener('ended', function() {
-        nextSong();
+        // Play next song if playlist isn't empty
+        if (playlist.length > 0) {
+            nextSong();
+        }
     });
     document.getElementById("songlist").innerHTML = html;
 }
