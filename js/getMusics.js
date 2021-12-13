77f10733561fa5109e325b6a916627df8b7e7dec
Xwilarg <xwilarg@yahoo.fr>
2021-12-14 00:01:41 +0100

    Improve shuffle code

--- js/getMusics.js
+++ js/getMusics.js
@@ -1,25 +1,46 @@
 let json;
-let remainingIndexs;
+
+// URL of the remote server
+// Left empty if stored at the same place as the webpage
 let url = "";
 
-function startSong(name, path) {
+// Next songs to play
+let playlist = [];
+
+// Play the next song of the playlist
+function nextSong() {
+    let elem = json[0];
+    playlist.shift();
+
     let player = document.getElementById("player");
-    player.src = url + path;
+    player.src = `${url}/data/${elem.path}`;
     player.play();
 
     let current = document.getElementsByClassName("current");
     if (current.length > 0) {
         current[0].classList.remove("current");
     }
-    document.getElementById(name).classList.add("current");
+    document.getElementById(sanitize(elem.name)).classList.add("current");
 }
 
-function playSong(name, path, index) {
-    remainingIndexs = [...Array(json.length).keys()];
-    remainingIndexs.splice(index, 1);
-    startSong(name, path);
+// Create a random playlist with the parameter as the first song
+function prepareShuffle(index) {
+    playlist.push(index);
+
+    let indexes = [...Array(json.length).keys()];
+    indexes.splice(index, 1);
+
+    // https://stackoverflow.com/a/46545530/6663248
+    playlist = playlist.concat(indexes
+        .map((value) => ({ value, sort: Math.random() }))
+        .sort((a, b) => a.sort - b.sort)
+        .map(({ value }) => value)
+    );
+
+    nextSong();
 }
 
+// Sanitize a name so the user can't inject HTML with the title
 function sanitize(text) {
     return text
         .replaceAll('&', '$amp;')
@@ -37,7 +58,7 @@ async function loadPage() {
     let index = 0;
     for (let elem of json) {
         html += `
-        <div class="song" id="${sanitize(elem.name)}" onclick="playSong('${sanitize(elem.name)}', '/data/${elem.path}', ${index})">
+        <div class="song" id="${sanitize(elem.name)}" onclick="prepareShuffle(${index})">
             <img src="${url + (elem.icon === undefined ? "/img/CD.png" : "/data/" + elem.icon)}"/><br/>
             ${sanitize(elem.name)}<br/>
             ${sanitize(elem.artist)}
@@ -46,13 +67,12 @@ async function loadPage() {
         index++;
     }
 
+    // Audio player config
     let player = document.getElementById("player");
-    player.volume = 0.1;
+    player.volume = 0.1; // Base volume is way too loud
+    // When song end, we start the next one
     player.addEventListener('ended', function() {
-        let rand = remainingIndexs[Math.floor(Math.random() * remainingIndexs.length)];
-        let item = json[rand];
-        startSong(item.name, "/data/" + item.path);
-        remainingIndexs.splice(rand, 1);
+        nextSong();
     });
     document.getElementById("songlist").innerHTML = html;
 }
