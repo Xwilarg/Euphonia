0dea08ad322a034bbad23ffde7ea948f24ac4145
Xwilarg <xwilarg@yahoo.fr>
2022-01-04 23:56:36 +0100

    Improve download script

--- scripts/youtube-download.py
+++ scripts/youtube-download.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import sys
 import glob
+from uuid import uuid4
 
 def is_audio(name):
     return name.endswith(".wav") or name.endswith(".mp3") or name.endswith("webm") or name.endswith(".mp4") or name.endswith(".mkv")
@@ -13,6 +14,7 @@ with open('../data/info.json', 'r', encoding='utf-8') as fd:
     data = json.load(fd)
 
 name = input("Enter the song name: ")
+path = name
 if any(x["name"] == name for x in data["musics"]):
     print("There is always a song with that name")
     exit(1)
@@ -22,9 +24,34 @@ if any(x["youtube"] == url for x in data["musics"]):
     print("There is already a song with the same URL")
     exit(1)
 
+artist = input("Enter the artist name: ")
+album = input("Enter the album name: ")
+
+id = str(uuid4())
+
 if not os.path.isdir("tmp"):
     os.mkdir("tmp")
-subprocess.run(["youtube-dl", url, "-o", "tmp/" + name], stderr=sys.stderr, stdout=sys.stdout)
-newName = [x for x in glob.glob("tmp/" + name + "*") if is_audio(x)][0]
+
+subprocess.run(["youtube-dl", url, "-o", "tmp/" + id], stderr=sys.stderr, stdout=sys.stdout)
+newName = [x for x in glob.glob("tmp/" + id + "*") if is_audio(x)][0]
 subprocess.run(["ffmpeg", "-i", newName, "../data/" + name + ".wav"], capture_output=True)
-subprocess.run(["rm", '"tmp/' + name + '".*'], capture_output=True)
+subprocess.run(["rm", newName], capture_output=True)
+
+data["musics"].append({
+    "name": name,
+    "path": path + ".wav",
+    "artist": artist,
+    "album": album,
+    "youtube": url
+})
+
+if album not in data["albums"]:
+    print("Missing album data, you'll need to add the image manually")
+    data["albums"][album] = {
+        "path": "TODO"
+    }
+
+with open('../data/info.json', 'r', encoding='utf-8') as fd:
+    json.dump(data, fd)
+
+print("Done!")
