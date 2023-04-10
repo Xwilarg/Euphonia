0d0eb7bf6fab3dd51a41cf3286a877dba9d3ca36
Xwilarg <xwilarg@protonmail.com>
2023-04-11 01:33:03 +0200

    Add button to set server data

--- Android/app/src/main/java/com/example/euphonia/MainActivity.kt
+++ Android/app/src/main/java/com/example/euphonia/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.euphonia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun updateData(view: View) {
+        var url = findViewById<EditText>(R.id.inputURL).text.toString()
+        if (!url.endsWith("/")) {
+            url += "/";
+        }
+        url += "php/getInfoJson.php"
+        Log.i("JSON", URL(url).readText())
     }
 }
