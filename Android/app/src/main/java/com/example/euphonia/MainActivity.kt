80dca2767b2d8c8428b7631ba25ca1185ee228bd
Xwilarg <xwilarg@protonmail.com>
2023-04-11 11:44:13 +0200

    Move download as background

--- Android/app/src/main/java/com/example/euphonia/MainActivity.kt
+++ Android/app/src/main/java/com/example/euphonia/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.euphonia
 
+import android.app.Notification
+import android.app.NotificationManager
 import android.content.Context
 import android.os.Bundle
 import android.os.Handler
@@ -9,10 +11,12 @@ import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import androidx.core.content.getSystemService
 import com.example.euphonia.data.MusicData
 import com.google.gson.Gson
@@ -39,33 +43,37 @@ class MainActivity : AppCompatActivity() {
 
         val list = findViewById<ListView>(R.id.musicData)
 
-        var dial: AlertDialog? = null
         val storageManager = this.getSystemService<StorageManager>()!!
+        val notificationManager = this.getSystemService<NotificationManager>()!!
         val uuid: UUID = storageManager.getUuidForPath(filesDir)
-        
+
+        val updateButton = findViewById<Button>(R.id.refreshData)
+        updateButton.isClickable = false
+        updateButton.alpha = .5f
+
         list.onItemClickListener = AdapterView.OnItemClickListener { parent, v, position, id ->
             Log.i("TEST", position.toString())
         }
 
+        val builder = NotificationCompat.Builder(this, "notify_download")
+            .setContentTitle("Updating data...")
+            .setSmallIcon(R.drawable.icon)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setOngoing(true)
+        notificationManager.notify(1, builder.build())
+
         executor.execute {
             val data = Gson().fromJson(URL("https://${url}php/getInfoJson.php").readText(), MusicData::class.java)
             val musics = data.musics.map { it.name }
-            val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, musics)
+            val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, musics)
 
             val files = this.fileList()
-
-            handler.post {
-                val downloadBar = AlertDialog.Builder(this)
-                downloadBar.setMessage("Updating data...")
-                dial = downloadBar.create()
-                dial!!.setCanceledOnTouchOutside(false)
-                dial!!.show()
-            }
 
             data.musics.forEachIndexed{ index, song ->
                 if (!files.contains(song.path)) {
                     this.openFileOutput(song.path, Context.MODE_PRIVATE).use {
-                        dial!!.setMessage("Updating data... $index / ${musics.size}")
+                        builder.setContentText("Updating data... $index / ${musics.size}")
+                        notificationManager.notify(1, builder.build())
                         URL("https://${url}data/normalized/${song.path}").openStream().use { stream ->
                             val bytes = stream.readBytes()
                             storageManager.allocateBytes(uuid, bytes.size.toLong())
@@ -76,8 +84,9 @@ class MainActivity : AppCompatActivity() {
             }
 
             handler.post {
-                dial!!.dismiss()
                 list.adapter = adapter
+                updateButton.isClickable = true
+                updateButton.alpha = 1f
             }
         }
     }
