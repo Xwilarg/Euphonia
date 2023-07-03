0f26747a74615a718229af862aa5bfce87784200
Christian Chaux <xwilarg@yahoo.fr>
2023-07-03 14:25:45 +0000

    Fix order

--- web/php/getAuthUrl.php
+++ web/php/getAuthUrl.php
@@ -24,8 +24,9 @@ else
 $credentials = json_decode(file_get_contents("../data/credentials.json"), true);
 
 // TODO: last.fm auth is such a massive pain and I don't want to spent 4 hours doing parsing for it
-$url = "api_key" . $credentials['apiKey'];
+$url = "";
 if (isset($_GET["album"])) $url .= "album" . $_GET["album"];
+$url .= "api_key" . $credentials['apiKey'];
 if (isset($_GET["artist"])) $url .= "artist" . $_GET["artist"];
 if (isset($_GET["duration"])) $url .= "duration" . $_GET["duration"];
 $url .= "method" . $_GET["method"];
