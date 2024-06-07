1c9ee932ab6f5b53125a8dc55e850695ebd8bb52
Xwilarg <xwilarg@protonmail.com>
2024-06-07 23:50:17 +0200

    Fix script path

--- scripts/normalize.ps1
+++ scripts/normalize.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "Stop"
 
-Get-ChildItem "../data/raw/" -Filter *.mp3 | 
+Get-ChildItem "../web/data/raw/" -Filter *.mp3 |
 Foreach-Object {
     ffmpeg-normalize "../web/data/raw/$($_)" -pr -ext mp3 -of ../web/data/normalized -c:a libmp3lame
 }
