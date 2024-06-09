60f8efa79de9ddebdd6b594972bd4f5619816013
Xwilarg <xwilarg@protonmail.com>
2024-06-09 19:07:21 +0200

    Start adding fields

---
+++ downloader/Downloader/Models/JsonExportData.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Downloader.Models
+{
+    public class JsonExportData
+    {
+        public List<string> Highlight { set; get; } = [];
+        public Dictionary<string, Playlist> Playlists { set; get; } = [];
+        public List<Song> Musics { set; get; } = [];
+        public Dictionary<string, Album> Albums { set; get; } = [];
+    }
+
+    public class Album
+    {
+        public string Path { set; get; }
+    }
+
+    public class Song
+    {
+        public string Name { set; get; }
+        public string Path { set; get; }
+        public string Artist { set; get; }
+        public string Album { set; get; }
+        public string Playlist { set; get; }
+        public string Source { set; get; }
+        public string Type { set; get; }
+    }
+
+    public class Playlist
+    {
+        public string Name { set; get; }
+        public string Description { set; get; }
+    }
+}
--- downloader/Downloader/ViewModels/MainViewModel.cs
+++ downloader/Downloader/ViewModels/MainViewModel.cs
@@ -1,8 +1,87 @@
-﻿namespace Downloader.ViewModels;
+﻿using Downloader.Models;
+using ReactiveUI;
+using System.IO;
+using System.Text.Json;
+
+namespace Downloader.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
     public MainViewModel()
     {
+        _jsonOptions = new()
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+
+        if (!Directory.Exists("data"))
+        {
+            Directory.CreateDirectory("data");
+            _data = new();
+        }
+        else
+        {
+            if (File.Exists("data/info.json"))
+            {
+                _data = JsonSerializer.Deserialize<JsonExportData>(File.ReadAllText("data/info.json"), _jsonOptions);
+            }
+            else
+            {
+                _data = new();
+            }
+        }
     }
+
+    private JsonExportData _data;
+    private JsonSerializerOptions _jsonOptions;
+
+    private string _songName;
+    public string SongName
+    {
+        get => _songName;
+        set => this.RaiseAndSetIfChanged(ref _songName, value);
+    }
+
+    private string _artist;
+    public string Artist
+    {
+        get => _artist;
+        set => this.RaiseAndSetIfChanged(ref _artist, value);
+    }
+
+    private string _url;
+    public string Url
+    {
+        get => _url;
+        set => this.RaiseAndSetIfChanged(ref _url, value);
+    }
+
+    private string _albumName;
+    public string AlbumName
+    {
+        get => _albumName;
+        set => this.RaiseAndSetIfChanged(ref _albumName, value);
+    }
+
+    private string _albumUrl;
+    public string AlbumUrl
+    {
+        get => _albumUrl;
+        set => this.RaiseAndSetIfChanged(ref _albumUrl, value);
+    }
+
+    private string _songType;
+    public string SongType
+    {
+        get => _songType;
+        set => this.RaiseAndSetIfChanged(ref _songType, value);
+    }
+
+    public string _playlist;
+    public string Playlist
+    {
+        get => _playlist;
+        set => this.RaiseAndSetIfChanged(ref _playlist, value);
+    }
+    public string[] PlaylistChoices { private set; get; }
 }
