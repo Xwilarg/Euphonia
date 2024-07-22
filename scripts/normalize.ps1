7c94ae64e8a85b32023781db8e5fa2d3ae1a97eb
Xwilarg <xwilarg@protonmail.com>
2024-07-22 19:57:41 +0200

    Remove unused scripts

--- scripts/normalize.ps1
+++
@@ -1,6 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-Get-ChildItem "../web/data/raw/" |
-Foreach-Object {
-    ffmpeg-normalize "../web/data/raw/$($_)" -pr -ext mp3 -of ../web/data/normalized -c:a libmp3lame
-}
--- scripts/normalize.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source ~/.profile
-set -e
-ffmpeg-normalize ../web/data/raw/* -pr -ext mp3 -of ../web/data/normalized -c:a libmp3lame
