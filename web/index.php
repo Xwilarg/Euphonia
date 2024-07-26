e87c493500ff98cd7f37524d606119c6b931ab8f
Xwilarg <xwilarg@protonmail.com>
2024-07-26 15:58:34 +0200

    Add playlist og tags

--- web/index.php
+++ web/index.php
@@ -8,7 +8,30 @@ use Twig\Environment;
 $loader = new FilesystemLoader(["templates"]);
 $twig = new Environment($loader);
 
+$rawInfo = file_get_contents("data/info.json");
+$info = json_decode($rawInfo, true);
+$metadata = json_decode(file_get_contents("data/metadata.json"), true);
+
+$name = $metadata["name"];
+$description = "";
+if (isset($_GET["playlist"]))
+{
+    $target = $_GET["playlist"];
+    foreach ($info["playlists"] as $key => $p)
+    {
+        if ($key == $target)
+        {
+            $name = $p["name"];
+            $description = $p["description"];
+        }
+    }
+}
+
 echo $twig->render("index.html.twig", [
-    "json" => file_get_contents("data/info.json"),
-    "metadata" => json_decode(file_get_contents("data/metadata.json"), true)
+    "json" => $rawInfo,
+    "metadata" => $metadata,
+    "og" => [
+        "name" => $name,
+        "description" => $description
+    ]
 ]);
