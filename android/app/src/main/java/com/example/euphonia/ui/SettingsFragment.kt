65dfc3d749f8e1b0f74a698936f9c4dad390bdb1
Xwilarg <xwilarg@protonmail.com>
2024-08-08 13:01:54 +0200

    Add website

--- android/app/src/main/java/com/example/euphonia/ui/SettingsFragment.kt
+++ android/app/src/main/java/com/example/euphonia/ui/SettingsFragment.kt
@@ -1,6 +1,10 @@
 package com.example.euphonia.ui
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.example.euphonia.R
 
@@ -8,5 +12,11 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+        findPreference<Preference>("website")!!.setOnPreferenceClickListener {
+            val sharedPref = requireContext().getSharedPreferences("settings", Context.MODE_PRIVATE)
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://${sharedPref.getString("remoteServer", null)}")))
+            true
+        }
     }
 }
