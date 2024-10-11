819c652acef6e56cd0c7c7f88fc4446ef83b4db2
Xwilarg <xwilarg@protonmail.com>
2024-10-11 14:50:19 +0200

    Preview youtube

--- web/src/upload.js
+++ web/src/upload.js
@@ -2,4 +2,17 @@ export async function upload_initAsync() {
     document.getElementById("upload").addEventListener("click", () => {
         document.getElementById("upload-window").hidden = !document.getElementById("upload-window").hidden;
     });
+
+    document.getElementById("upload-form").addEventListener("submit", (e) => {
+        e.preventDefault();
+        console.log(e);
+    });
+
+    document.getElementById("upload-url").addEventListener("change", (e) => {
+        const r = /youtu\.?be(\.com)?\/[^\?]+\?(v|si)=([^&]+)/g.exec(e.target.value);
+        if (r !== null)
+        {
+            document.getElementById("upload-yt-player").src = `https://www.youtube-nocookie.com/embed/${r[3]}`;
+        }
+    });
 }
