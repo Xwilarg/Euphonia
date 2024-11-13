6a0447da8ce7f5de7261ab54ffe73503c0ef854f
Xwilarg <xwilarg@protonmail.com>
2024-11-13 18:45:03 +0100

    Improve navbar display

--- web/src/main/navbar.js
+++ web/src/main/navbar.js
@@ -1,15 +1,13 @@
 export async function navbar_initAsync()
 {
-    /*
     document.getElementById("toggle-settings").addEventListener("click", () => {
-        const settings = document.getElementById("settings");
-        if (settings.classList.contains("is-active")) settings.classList.remove("is-active");
-        else settings.classList.add("is-active");
+        const settings = document.getElementById("settings-dropdown");
+        if (settings.classList.contains("is-hidden")) settings.classList.remove("is-hidden");
+        else settings.classList.add("is-hidden");
     });
     document.getElementById("toggle-volume").addEventListener("click", () => {
-        const settings = document.getElementById("volume-container");
-        if (settings.classList.contains("is-active")) settings.classList.remove("is-active");
-        else settings.classList.add("is-active");
+        const settings = document.getElementById("volume-container-dropdown");
+        if (settings.classList.contains("is-hidden")) settings.classList.remove("is-hidden");
+        else settings.classList.add("is-hidden");
     });
-    */
 }
