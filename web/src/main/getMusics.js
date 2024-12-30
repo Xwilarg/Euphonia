dc526640d163749bb6ab2a425b7320bd1cca8f82
Xwilarg <xwilarg@protonmail.com>
2024-12-30 15:09:23 +0100

    Move song code to separate file

--- web/src/main/getMusics.js
+++ web/src/main/getMusics.js
@@ -7,6 +7,7 @@
 import * as wanakana from 'wanakana';
 import { registerNowPlayingAsync, registerScrobbleAsync } from "./lastfm"
 import { archiveSong, getApiToken, isLoggedIn, logOff, repairSong, updateSong, validateIntegrity } from '../common/api';
+import { spawnSongNode } from './song';
 
 let json;
 let metadataJson;
@@ -349,78 +350,7 @@ function displaySongs(musics, id, filter, doesSort, doesShuffle, count) {
     } else {
         document.getElementById(id).innerHTML = "";
         for (let elem of musics) {
-
-            const curr = elem;
-            let template = document.getElementById("template-song");
-            const node = template.content.cloneNode(true);
-
-            let idContainer = `${curr.id}-${id}`;
-            node.querySelector(".song").id = idContainer;
-            // If we are in minimalist mode, we allow click anywhere on the song since only the text part is displayed
-            node.querySelector(".song-img").onclick = () => {
-                if (!isMinimalist) {
-                    prepareShuffle(curr.id);
-                }
-            };
-            // If we are not, we only allow click on the image so user can copy text without starting song
-            node.querySelector(".song").onclick = () => {
-                if (isMinimalist) {
-                    prepareShuffle(curr.id);
-                }
-            }
-            let target = node.querySelector(".edit-content");
-            let form = node.querySelector("form");
-            node.querySelector(".song-edit").addEventListener("click", () => {
-                target.hidden = !target.hidden;
-                if (!target.hidden) {
-                    form.reset();
-
-                    for (var i = 0, len = form.elements.length; i < len; ++i) {
-                        form.elements[i].disabled = false;
-                    }
-                    form.getElementsByClassName("edit-source")[0].value = elem.source;
-                }
-            });
-            node.querySelector(".song-repair").addEventListener("click", () => {
-                if (isRepairOngoing) {
-                    alert("A repair is already ongoing")
-                } else {
-                    let res = confirm("Are you sure you want to repair this song?");
-                    if (res) {
-                        isRepairOngoing = true;
-                        repairSong(getSongKey(elem), () => {
-                            isRepairOngoing = false;
-                        }, () => {
-                            isRepairOngoing = false;
-                        });
-                    }
-                }
-            })
-            form.addEventListener("submit", (e) => {
-                e.preventDefault();
-                
-                const data = new FormData(e.target);
-                data.append("Key", getSongKey(curr));
-
-                for (var i = 0, len = form.elements.length; i < len; ++i) {
-                    form.elements[i].disabled = true;
-                }
-                updateSong(data, () => {
-                    target.hidden = true;
-
-                    if (data["Tags"] !== undefined) {
-                        curr.tags = data["Tags"];
-                    }
-
-                    updateSingleSongDisplay(document.getElementById(idContainer), curr);
-                }, () => {
-                    target.hidden = true;
-                });
-            });
-
-            updateSingleSongDisplay(node, curr);
-            document.getElementById(id).appendChild(node);
-            updateSingleSongDisplay(document.getElementById(idContainer), curr);
+            spawnSongNode(elem);
         }
 
         // Playlist changed, maybe there is a song we should highlight now
---
+++ web/src/main/song.js
@@ -0,0 +1,74 @@
+// module "song.js"
+
+export function spawnSongNode(curr) {
+    let template = document.getElementById("template-song");
+    const node = template.content.cloneNode(true);
+
+    let idContainer = `${curr.id}-${id}`;
+    node.querySelector(".song").id = idContainer;
+    // If we are in minimalist mode, we allow click anywhere on the song since only the text part is displayed
+    node.querySelector(".song-img").onclick = () => {
+        if (!isMinimalist) {
+            prepareShuffle(curr.id);
+        }
+    };
+    // If we are not, we only allow click on the image so user can copy text without starting song
+    node.querySelector(".song").onclick = () => {
+        if (isMinimalist) {
+            prepareShuffle(curr.id);
+        }
+    }
+    let target = node.querySelector(".edit-content");
+    let form = node.querySelector("form");
+    node.querySelector(".song-edit").addEventListener("click", () => {
+        target.hidden = !target.hidden;
+        if (!target.hidden) {
+            form.reset();
+
+            for (var i = 0, len = form.elements.length; i < len; ++i) {
+                form.elements[i].disabled = false;
+            }
+            form.getElementsByClassName("edit-source")[0].value = elem.source;
+        }
+    });
+    node.querySelector(".song-repair").addEventListener("click", () => {
+        if (isRepairOngoing) {
+            alert("A repair is already ongoing")
+        } else {
+            let res = confirm("Are you sure you want to repair this song?");
+            if (res) {
+                isRepairOngoing = true;
+                repairSong(getSongKey(elem), () => {
+                    isRepairOngoing = false;
+                }, () => {
+                    isRepairOngoing = false;
+                });
+            }
+        }
+    })
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const data = new FormData(e.target);
+        data.append("Key", getSongKey(curr));
+
+        for (var i = 0, len = form.elements.length; i < len; ++i) {
+            form.elements[i].disabled = true;
+        }
+        updateSong(data, () => {
+            target.hidden = true;
+
+            if (data["Tags"] !== undefined) {
+                curr.tags = data["Tags"];
+            }
+
+                updateSingleSongDisplay(document.getElementById(idContainer), curr);
+        }, () => {
+            target.hidden = true;
+        });
+    });
+
+    updateSingleSongDisplay(node, curr);
+    document.getElementById(id).appendChild(node);
+    updateSingleSongDisplay(document.getElementById(idContainer), curr);
+}
