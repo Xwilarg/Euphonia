4905d1757e6eab3eec1d302e717605c1ad770501
Xwilarg <xwilarg@protonmail.com>
2025-02-25 13:35:31 +0100

    Add upload form

--- extension/js/index.js
+++ extension/js/index.js
@@ -14,11 +14,70 @@ async function initAsync() {
                 document.getElementById("choose-password").classList.remove("is-hidden");
             } else {
                 token = result.token;
-                fetch(`${website}?json=1`)
+                fetch(`${website}?json=1`) // Get JSON info
                 .then(resp => resp.ok ? resp.json() : Promise.reject(`Code ${resp.status}`))
                 .then(json => {
                     document.getElementById("choose-loading").classList.add("is-hidden");
                     document.getElementById("choose-upload").classList.remove("is-hidden");
+
+                    // TODO: dupplicated code from web
+
+                    if (json.playlists) { // Upload available playlists
+                        for (let [key, value] of Object.entries(json.playlists)) {
+                            document.getElementById("upload-playlist").innerHTML += `<option value="${key}">${value.name}</option>`;
+                        }
+                    }
+
+                    // Code from web/src/upload.js
+                    document.getElementById("upload-url").addEventListener("change", (_) => {
+                        const upload = document.getElementById("upload-url");
+                        const content = upload.value;
+                        if (content === "") {
+                            document.getElementById("upload-url-error").classList.add("is-hidden");
+                            upload.classList.remove("is-danger");
+                        } else {
+                            const r = /youtu\.?be(\.com)?\/(watch\?v=)?([^?]+)/g.exec(upload.value);
+                            if (r === null) {
+                                document.getElementById("upload-url-error").classList.remove("is-hidden");
+                                upload.classList.add("is-danger");
+                            } else {
+                                document.getElementById("upload-url-error").classList.add("is-hidden");
+                                upload.classList.remove("is-danger");
+                            }
+                        }
+                    });
+
+                    document.getElementById("upload-form").addEventListener("submit", (e) => {
+                        e.preventDefault();
+                        const data = new FormData(e.target);
+
+                        document.getElementById("choose-loading").classList.add("is-hidden");
+                        document.getElementById("choose-upload").classList.remove("is-hidden");
+                        fetch(`${apiTarget}api/download/upload`, {
+                            method: 'POST',
+                            headers: {
+                                'Authorization': `Bearer ${token}`
+                            },
+                            body: data
+                        })
+                        .then(resp => resp.ok ? resp.json() : Promise.reject(`Code ${resp.status}`))
+                        .then(json => {
+                            if (json.success) {
+                                alert(`Upload is on the way`);
+                                document.getElementById("choose-loading").classList.remove("is-hidden");
+                                document.getElementById("choose-upload").classList.add("is-hidden");
+                            } else {
+                                alert(`Failed to upload song: ${err}`);
+                                document.getElementById("choose-loading").classList.remove("is-hidden");
+                                document.getElementById("choose-upload").classList.add("is-hidden");
+                            }
+                        })
+                        .catch((err) => {
+                            alert(`Failed to upload song: ${err}`);
+                            document.getElementById("choose-loading").classList.remove("is-hidden");
+                            document.getElementById("choose-upload").classList.add("is-hidden");
+                        });
+                    });
                 })
                 .catch((err) => {
                     alert("Failed to get website info")
