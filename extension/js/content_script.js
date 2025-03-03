509173800d2391c4830b39b0aab0c559b71409a9
Xwilarg <xwilarg@protonmail.com>
2025-03-03 16:28:16 +0100

    Update extension to work with Youtube music

--- extension/js/content_script.js
+++ extension/js/content_script.js
@@ -6,12 +6,24 @@ chrome.runtime.onMessage.addListener(
             try
             {
                 url = location.href;
-                artist = document.querySelector("#text").title;
-                name = document.querySelector("#title yt-formatted-string.ytd-watch-metadata").innerHTML;
-                const linkTarget = document.querySelector("#shelf-container .yt-video-attribute-view-model__secondary-subtitle > span");
-                const linkName = linkTarget.querySelector("a");
-                albumName = linkName ? linkName.innerHTML : linkTarget.innerHTML;
-                albumImage = document.querySelector("#shelf-container img").src;
+
+                if (url.startsWith("https://www.youtube.com"))
+                {
+                    artist = document.querySelector("#text").title;
+                    name = document.querySelector("#title yt-formatted-string.ytd-watch-metadata").innerHTML;
+                    const linkTarget = document.querySelector("#shelf-container .yt-video-attribute-view-model__secondary-subtitle > span");
+                    const linkName = linkTarget.querySelector("a");
+                    albumName = linkName ? linkName.innerHTML : linkTarget.innerHTML;
+                    albumImage = document.querySelector("#shelf-container img").src;
+                }
+                else
+                {
+                    const midControls = document.querySelector(".middle-controls");
+                    artist = midControls.querySelector(".subtitle a").innerHTML;
+                    name = midControls.querySelector(".title").title;
+                    albumName = midControls.querySelectorAll(".subtitle a")[1].innerHTML;
+                    albumImage = document.querySelector("#song-image img").src;
+                }
             }
             finally
             {
